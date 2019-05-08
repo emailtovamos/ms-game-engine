71490edb697663b819c4517c949c3b8a76adc37f
Satyajit <emailtovamos@gmail.com>
2019-05-08 18:41:03 +0100

    Proper logic which takes past 4 scores into account

--- internal/server/logic/logic.go
+++ internal/server/logic/logic.go
@@ -1,12 +1,28 @@
 package logic
 
+import (
+	"fmt"
+)
+
 var pastFourScores = []float64{5.0, 4.0, 2.0, 1.0}
 
 func GetSize() float64 {
-	if pastFourScores[0]+pastFourScores[1] < pastFourScores[2]+pastFourScores[3] {
-		return 600.0
+	fmt.Println(pastFourScores)
+	olderScores := pastFourScores[0] + pastFourScores[1]
+	recentScores := pastFourScores[2] + pastFourScores[3]
+
+	diff := recentScores - olderScores
+	if diff > 0.0 {
+		size := 600.0 + diff*60.0
+		if size < 2000.0 {
+			return size
+		}
+		return 2000.0
 	}
-	return 100.0
+	if diff > -5.0 && diff <= 0.0 {
+		return 100.0 + 18.0*diff
+	}
+	return 10.0
 }
 
 func Average() float64 {
